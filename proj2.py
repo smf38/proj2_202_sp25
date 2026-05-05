a9ceb291c82fc466e098e0ae1039004ddfb9c717
smf38 <smitch38@calpoly.edu>
2026-05-04 19:25:03 -0700

    task two complete, parse function

--- proj2.py
+++ proj2.py
@@ -1,7 +1,7 @@
 import csv
-import math
-from dataclasses import dataclass
-from typing import *
+from typing import Optional
+import sys
+sys.setrecursionlimit(10_000)
 #testing pycharm commits
 
 # Put your data definitions first!
@@ -24,5 +24,47 @@ class Node:
 # ...
 
 # Then your functions.
+def read_csv_lines(filename: str) -> Optional[Node]:
+    def parse_row(fields: list[str]) -> Row:
+        country = str(fields[0])
+        year =  int(fields[1])
+        electricity_and_heat_co2_emissions = float(fields[2])
+        electricity_and_heat_co2_emissions_per_capita = float(fields[3])
+        energy_co2_emissions = float(fields[4])
+        energy_co2_emissions_per_capita = float(fields[5])
+        total_co2_emissions_excluding_lucf =  float(fields[6])
+        total_co2_emissions_excluding_lucf_per_capita = float(fields[7])
+        return Row(country,
+                    year,
+                    electricity_and_heat_co2_emissions,
+                    electricity_and_heat_co2_emissions_per_capita,
+                    energy_co2_emissions,
+                    energy_co2_emissions_per_capita,
+                    total_co2_emissions_excluding_lucf,
+                    total_co2_emissions_excluding_lucf_per_capita)
+    with open(filename, mode='r', newline="") as file:
+        reader = csv.reader(file)
+        rows = list(reader)
+    correct_header = ['country',
+                    'year',
+                    'electricity_and_heat_co2_emissions',
+                    'electricity_and_heat_co2_emissions_per_capita',
+                    'energy_co2_emissions',
+                    'energy_co2_emissions_per_capita',
+                    'total_co2_emissions_excluding_lucf',
+                    'total_co2_emissions_excluding_lucf_per_capita']
+    if len(rows) == 0:
+        return None
+    if rows[0] != correct_header:
+        return None
+
+    def create_linked_list(idx:int) -> Optional[Node]:
+        if idx == len(rows):
+            return None
+        current = parse_row(rows[idx])
+        return Node(current, create_linked_list(idx+1))
+
+    return create_linked_list(1)
+
 
 # ...
